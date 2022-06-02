bd402cd69b094a2543271e5d9996c7a0b2f26ae4
Ariceta <mikel.ariceta@nexxyolabs.com>
2022-06-02 09:03:26 +0200

    Add scrpit to interact with testnet

--- contracts/FirstToken.sol
+++ contracts/FirstToken.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FirstToken is ERC20, ERC20Burnable, Ownable{
+contract FirstToken is ERC20, ERC20Burnable, Ownable {
 
-    constructor(uint256 initialSupply_) ERC20("Token", "TOK")  {
-        _mint(msg.sender, initialSupply_);
+    constructor(uint256 initialSupply) ERC20("Token", "TOK") {
+        _mint(msg.sender, initialSupply);
     }
 
-    function mint(uint256 amount) public onlyOwner{
+    function mint(uint256 amount) public onlyOwner {
         _mint(msg.sender, amount);
     }
 }
--- scripts/example.ts
+++ scripts/example.ts
@@ -1,6 +1,7 @@
 import { deployments, getNamedAccounts, ethers } from "hardhat";
 import { FirstToken } from "../typechain";
 import { Deployment } from "hardhat-deploy/types";
+import { BigNumber, ContractTransaction } from "ethers";
 
 const main = async () => {
   const { deployer } = await getNamedAccounts();
@@ -12,6 +13,19 @@ const main = async () => {
     token.address
   );
   console.log(await contractBase.address);
+  let totalSupply: BigNumber = BigNumber.from(await contractBase.totalSupply());
+  console.log(totalSupply);
+
+  let amountToBurn: BigNumber = ethers.utils.parseUnits("20", "ether");
+  let tx: ContractTransaction = await contractBase.burn(amountToBurn);
+  console.log(tx);
+  totalSupply = BigNumber.from(await contractBase.totalSupply());
+  console.log(totalSupply.toString());
+
+  let amountToMint: BigNumber = ethers.utils.parseUnits("50", "ether");
+  await contractBase.mint(amountToMint);
+  totalSupply = BigNumber.from(await contractBase.totalSupply());
+  console.log(totalSupply);
 };
 
 main();
